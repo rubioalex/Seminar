fa3d794f7e2e9c0f5f100081984219773f2d3723
rubioalex <123464660+rubioalex@users.noreply.github.com>
2023-04-18 08:38:19 +0000

    Tue, Apr 18, 2023, 11:38 AM +03:00

--- exchange_values.c
+++ exchange_values.c
@@ -5,6 +5,8 @@ int main(void)
 
 int x = 28;
 int y = 50;
+int z = 125;
+// What about comments?
 
 // swapping...
 int temp = x;
@@ -13,6 +15,7 @@ y = temp;
 
 printf("x is now %i\n", x);
 printf("y is now %i\n", y);
+printf("Adding another line in which z = %i\n", z);
 
 // This is just a comment
 // This is another comment
